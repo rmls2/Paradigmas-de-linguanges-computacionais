3eeb68dd523bb88e48db8d5db0303f747546aaa8
rmls2 <millerrobert182@gmail.com>
2024-06-16 21:10:09 -0300

    atualizacao q1-plc_prova

--- exercicios_resolvidos/q1-plc_prova1.hs
+++ exercicios_resolvidos/q1-plc_prova1.hs
@@ -1,3 +1,4 @@
+--q1
 functa:: [Int] -> [Int]
 functa [x] = []
 functa (x:y:ys) = if x == y then x: functa(y:ys) else functa(y:ys)
@@ -7,17 +8,31 @@ functa (x:y:ys) = if x == y then x: functa(y:ys) else functa(y:ys)
  queremos: 
 l =  [1,2,3,3,4]
 
-f (1:2:[3,3,4]) = f(2:[3,3,4])
-f(2:[3,3,4]) = f([3:[3,4]])
-f([3:[3,4]]) = 3:f(3,4) = 3:(f(3:[])) = 3:[] = [3]
-
-lista dos repetidos = [ x | x <- (x:y:ys), x==y] 
-
--}
+= 1 == 2 f [2,3,3,4]]
+= f [2,3,3,4]] = 2 == 3 f [3,3,4]
+= f [3,3,4]  = 3 == 3 3:f[3,4] = 3:(f[4]) = 3:[] = [3] -}
 
 functaComprehension :: [Int] -> [Int]
 functaComprehension [] = []
 functaComprehension xs = [xs !! i | i <- [0..length xs - 2], xs !! i == xs !! (i + 1)]
 
 
+mfoldr :: (t -> Bool -> Bool) -> Bool -> [t] -> Bool
+mfoldr         f           v    [ ]  = v
+mfoldr         f           v   (x:xs) = f x (mfoldr f v xs)
+ 
 
+--Q 2
+
+--Recursão
+testaElementos :: (a -> Bool) -> [a] -> Bool
+testaElementos _ [] = True
+testaElementos f (x:xs) = f x && testaElementos f xs
+
+--map e and
+testaElementos2 :: (a -> Bool) -> [a] -> Bool
+testaElementos2 f xs = and (map f xs)
+
+--foldr
+testaElementos3 :: (a -> Bool) -> [a] -> Bool
+testaElementos3 f = foldr (\x acc -> f x && acc) True
