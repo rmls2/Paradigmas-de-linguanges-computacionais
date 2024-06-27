e405d7c9f43caf059c33a191bcccd1bfbad6d189
rmls2 <millerrobert182@gmail.com>
2024-06-27 11:37:25 -0300

    feat: aula 27/06/24

---
+++ java/Banco.java
@@ -0,0 +1,9 @@
+public class Banco {
+    public static void main(String[] args) {
+        Conta conta;
+
+        conta = new Conta("123");
+        System.out.println(".()" + conta.numero);
+    }
+
+}
---
+++ java/Conta.java
@@ -0,0 +1,15 @@
+public class Conta {
+
+    public double saldo;
+    public String numero;
+
+    public Conta(String numero){
+        this.saldo = 0.0;
+        this.numero = numero;
+    }
+
+
+    }
+
+
+
